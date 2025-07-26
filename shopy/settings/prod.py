967a79bc067d8e23546d6e0753f33700bad908a5
mostafa <mostafaa7med1000@gmail.com>
2025-07-26 14:27:30 +0300

    adding DB configuration

--- shopy/settings/prod.py
+++ shopy/settings/prod.py
@@ -7,4 +7,10 @@ ADMINS = [("admin", "admin@test.com")]
 
 ALLOWED_HOSTS = ["*"]
 
-DATABASES = {"default": {}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
