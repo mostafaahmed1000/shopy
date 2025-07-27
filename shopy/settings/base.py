40f653ce9895fe739d453c72ba9c3ea6ce7b905b
Mostafa Ahmed Mostafa <mostafaa7med1000@gmail.com>
2025-07-27 17:26:07 +0300

    fixing media root

--- shopy/settings/base.py
+++ shopy/settings/base.py
@@ -137,7 +137,7 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 urlpatterns = [
-] + static(settings.MEDIA_URL, document_root=settings.base.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 CART_SESSION_ID = "cart"
 
