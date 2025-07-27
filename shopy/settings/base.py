9fdd4daa01ad0aa4f210520f9da336d463e7112e
Mostafa Ahmed Mostafa <mostafaa7med1000@gmail.com>
2025-07-27 14:03:01 +0300

    change media root

--- shopy/settings/base.py
+++ shopy/settings/base.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CART_SESSION_ID = "cart"
 
